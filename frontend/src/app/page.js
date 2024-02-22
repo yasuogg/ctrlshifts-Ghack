0d7f46cf5fb420aff4ea9022245ad69f88062cad
yasuogg <sohaibben52@gmail.com>
2024-02-22 14:10:46 +0000

    landing page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,21 @@
+import Header from "@/components/landing-page/header";
+import Hero from "@/components/landing-page/hero";
+import Squares from "@/components/landing-page/squares";
+import Looking from "@/components/landing-page/team-looking";
+import Timeline from "@/components/landing-page/timline-hero";
+import Workflow from "@/components/landing-page/workflow";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-center">hello world</div>
+    <>
+    <Header/>
+    <Hero/>
+    <Timeline/>
+    <Squares/>
+    <Workflow/>
+    <Looking/>
+    </>
+
   );
 }
